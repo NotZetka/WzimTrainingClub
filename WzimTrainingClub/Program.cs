5418f43e6327c4db0c93e0cb05cabd5e60488046
Zetka <jakub.kochanski10@gmail.com>
2024-03-20 21:42:10 +0100

    populated database with tables

--- WzimTrainingClub/Program.cs
+++ WzimTrainingClub/Program.cs
@@ -9,22 +9,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-var services = app.Services;
-try
+using (var scope = app.Services.CreateScope())
 {
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.Migrate();
-}
-catch (Exception ex)
-{
-    var logger = services.GetService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
-}
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
 }
 
 app.UseHttpsRedirection();
