22cb8df35e2376de23ef04377277acfaf3f4c766
Patryk Całka <patrykcalka@protonmail.com>
2024-04-23 21:48:21 +0200

    Added GoalViewModel ViewModel.

---
+++ WzimTrainingClub/ViewModels/GoalViewModel.cs
@@ -0,0 +1,9 @@
+﻿using WzimTrainingClub.Models;
+
+namespace WzimTrainingClub.ViewModels;
+
+public class GoalViewModel
+{
+    public Goal Goal { get; set; }
+    public GoalProgress[] Progress { get; set; }
+}
