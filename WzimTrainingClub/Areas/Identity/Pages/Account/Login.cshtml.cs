e486ef66509dc2cf7b1a8c314dadc17ea2c3153d
Dawid Walczak <dawidwalczak1404@gmail.com>
2024-04-27 18:11:59 +0200

    fix labels and error messages in login and register forms

--- WzimTrainingClub/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WzimTrainingClub/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -42,9 +42,10 @@ namespace WzimTrainingClub.Areas.Identity.Pages.Account
 
             [Required]
             [DataType(DataType.Password)]
+            [Display(Name = "Hasło")]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Zapamiętaj mnie")]
             public bool RememberMe { get; set; }
         }
 
