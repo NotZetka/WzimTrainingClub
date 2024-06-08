a24c3f5fe0fe97e05ea336478b89698d44ddb4c7
Patryk Całka <patrykcalka@protonmail.com>
2024-06-08 13:52:48 +0200

    Added EditGoalInputModel ViewModel.

---
+++ WzimTrainingClub/ViewModels/EditGoalInputModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WzimTrainingClub.ViewModels;
+
+public class EditGoalInputModel
+{
+    [Range(0,double.MaxValue)]
+    public int ID { get; set; }
+    [Required]
+    [MaxLength(30)]
+    public string Activity { get; set; }
+    [Required]
+    [MaxLength(20)]
+    public string Type { get; set; }
+    [Range(0,500)]
+    public float Weight { get; set; }
+    [Range(0,100)]
+    public int Reps { get; set; }
+    [Range(0,24)]
+    public int Hours { get; set; }
+    [Range(0,60)]
+    public int Minutes { get; set; }
+    [Range(0,60)]
+    public int Seconds { get; set; }
+    [Range(0,double.MaxValue)]
+    public float Quantity { get; set; }
+    [MaxLength(30)]
+    public string QuantityUnit { get; set; }
+}
